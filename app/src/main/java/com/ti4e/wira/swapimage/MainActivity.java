52bc8a77d8b67a584e29224827c488350f854e91
syafri wira wicaksana <wirasyafri1998@gmail.com>
2018-10-21 10:22:46 +0700

    bismillah

---
+++ app/src/main/java/com/ti4e/wira/swapimage/MainActivity.java
@@ -0,0 +1,50 @@
+package com.ti4e.wira.swapimage;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Toast;
+
+
+public class MainActivity extends AppCompatActivity implements NabiListFragment.Listener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+
+//    public void onShowDetails(View view){
+//        Intent intent = new Intent(this, DetailActivity.class);
+//        startActivity(intent);
+//    }
+
+    public void itemClicked(long id)
+    {
+        View fragmenDetail = findViewById(R.id.detailingFrag);
+        if(fragmenDetail!=null)
+        {
+            NabiDetailFragment deta = new NabiDetailFragment();
+
+            FragmentTransaction fragTrans = getSupportFragmentManager().beginTransaction();
+            deta.setResep(id);
+            fragTrans.replace(R.id.detailingFrag,deta);
+            fragTrans.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE);
+            //fragTrans.addToBackStack(null);
+            fragTrans.commit();
+        }
+
+        else
+        {
+            Toast.makeText(this, "Item " + id + " was clicked", Toast.LENGTH_SHORT).show();
+            Intent intent = new Intent(this.getApplicationContext(), DetailActivity.class);
+            Bundle b = new Bundle();
+            b.putLong("id",id);
+            intent.putExtras(b);
+            startActivity(intent);
+        }
+    }
+}
